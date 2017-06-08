6043c08d89fc0f33d0e8705a7ab6ea714de28c61
kervin5 <kervinlvh@gmail.com>
2017-06-07 22:28:02 -0700

    Implement AJAX call and display results

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+function isExpanded() {
+    return $(".input-holder").width() > 200;
+}
+
 //Javascript
 
 function searchToggle(obj, evt){
@@ -16,7 +20,44 @@ function searchToggle(obj, evt){
 
 $(".search-icon").click(function () {
     var wrapper = $(".search-wrapper");
-    if(wrapper.hasClass("active") && $(".input-holder").width() > 200){
+    if(wrapper.hasClass("active") && isExpanded()){
         wrapper.addClass("move-up");
     }
+});
+
+// Wikipedia API call
+
+$(document).ready(function() {
+
+    $(".search-icon").on("click", function() {
+        if(isExpanded()){
+            $(".results").empty();
+
+            //gets input and put it in variable
+            var searchquery = document.getElementById("wiki-search").value;
+
+            //output into div
+            $(".value").html("Your Search Value: " + searchquery);
+
+            var link = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=" + searchquery + "&limit=10&callback=?";
+
+            //output link
+            $(".srlink").html("Link: " + link);
+
+            $.getJSON(link, function(data) {
+                $(".raw").html(JSON.stringify(data[1]));
+                console.log(data);
+                /*data[0] = searched;
+                 data[1] = title;
+                 data[2] = snippet;
+                 data[3] = link;
+                 */
+
+                for(var i =0; i < data[1].length; i++) {
+                    $(".results").prepend('<div class="wiki-result">' + '<a href =' + data[3][i] + ' target=_blank>' + '<h1>' + data[1][i] + '</h1></a>' + '<h3>' + data[2][i] +'</h3></div>' );
+
+                }
+            });
+        }
+    });
 });
